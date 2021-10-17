06ab3e4314d46fb46284766c67c63602769c90ee
Masełko Michał <michal.maselko@microsoft.wsei.edu.pl>
2021-10-17 12:37:45 +0200

    Added alternative ways of passing data to Views

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -12,8 +12,12 @@ namespace LibApp.Controllers
         public IActionResult Random()
         {
             var firstBook = new Book(){Name = "English dictionary" };
+            var secondBook = new Book() { Name = "German dictionary" };
 
-            return View(firstBook);
+            ViewBag.Book1 = firstBook;
+            ViewData["Book2"] = secondBook;
+
+            return View();
         }
 
         public IActionResult Edit(int bookId)
