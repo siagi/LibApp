e69809de468f6da3191ce354cc26a28132bde164
michal.maselko <michal.maselko@mirosoft.wsei.edu.pl>
2021-11-14 12:12:21 +0100

    changes

---
+++ Data/Migrations/20211114110503_SetNameOfMembershipTypes.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibApp.Data.Migrations
+{
+    public partial class SetNameOfMembershipTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Membership SET Name = 'Pay as you go' WHERE Id = 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Models/MembershipType.cs
+++ Models/MembershipType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,9 @@ namespace LibApp.Models
 {
     public class MembershipType
     {
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
         public byte Id { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
